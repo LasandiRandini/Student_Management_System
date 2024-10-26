0938758734ab6aceed02d85b4fa5ce0961ca74a8
Janendra <131513745+Janendra2001@users.noreply.github.com>
2024-10-26 19:31:23 +0530

    commit

--- backend/routes/dashboard_route.js
+++ backend/routes/dashboard_route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getDashboardMetrics, searchDepartment } from "../controllers/department_controller.js";
+import { getDashboardMetrics, searchDepartment, getDepartmentWiseData } from "../controllers/department_controller.js";
 import { searchModule } from "../controllers/module_controller.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/dashboardMetrics", getDashboardMetrics);
 router.get("/searchDepartment/:departmentName", searchDepartment);
 router.get("/searchModule/:moduleName", searchModule);
+router.get("/departmentWiseData", getDepartmentWiseData);
 
 export default router;
